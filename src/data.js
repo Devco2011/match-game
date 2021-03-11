7a508dbaaaf365af133d14f24e684b041bc1484c
Devco2011 <devco2011@gmail.com>
2021-03-11 11:31:12 -0600

    data

---
+++ src/data.js
@@ -0,0 +1,149 @@
+export default function getLanguageCards() {
+    return [
+        {
+            id: 1,
+            name: "Apple Bloom",
+            image: "AppleBloom.png",
+            foundMatch: false
+        },
+        {
+            id: 2,
+            name: "Apple Bloom",
+            image: "AppleBloom.png",
+            foundMatch: false
+        },
+
+        {
+            id: 3,
+            name: "Apple Jack",
+            image: "Applejack.png",
+            foundMatch: false
+        },
+        {
+            id: 4,
+            name: "Apple Jack",
+            image: "Applejack.png",
+            foundMatch: false
+        },
+        {
+            id: 5,
+            name: "Flutter Shy",
+            image: "Fluttershy.png",
+            foundMatch: false
+        },
+        {
+            id: 6,
+            name: "Flutter Shy",
+            image: "Fluttershy.png",
+            foundMatch: false
+        },
+        {
+            id: 7,
+            name: "Pinkie Pie",
+            image: "PinkiePie.png",
+            foundMatch: false
+        },
+        {
+            id: 8,
+            name: "Pinkie Pie",
+            image: "PinkiePie.png",
+            foundMatch: false
+        },
+        {
+            id: 9,
+            name: "Princess Celestia",
+            image: "PrincessCelestia.jpg",
+            foundMatch: false
+        },
+        {
+            id: 10,
+            name: "Princess Celestia",
+            image: "PrincessCelestia.jpg",
+            foundMatch: false
+        },
+        {
+            id: 11,
+            name: "Princess Luna",
+            image: "PrincessLuna.png",
+            foundMatch: false
+        },
+        {
+            id: 12,
+            name: "Princess Luna",
+            image: "PrincessLuna.png",
+            foundMatch: false
+        },
+        {
+            id: 13,
+            name: "Twilight Sparkle",
+            image: "PrincessTwilightSparkle.png",
+            foundMatch: false
+        },
+        {
+            id: 14,
+            name: "Twilight Sparkle",
+            image: "PrincessTwilightSparkle.png",
+            foundMatch: false
+        },
+        {
+            id: 15,
+            name: "Rainbow Dash",
+            image: "RainbowDash.jpeg",
+            foundMatch: false
+        },
+        {
+            id: 16,
+            name: "Rainbow Dash",
+            image: "RainbowDash.jpeg",
+            foundMatch: false
+        },
+        {
+            id: 17,
+            name: "Rarity",
+            image: "Rarity.png",
+            foundMatch: false
+        },
+        {
+            id: 18,
+            name: "Rarity",
+            image: "Rarity.png",
+            foundMatch: false
+        },
+        {
+            id: 19,
+            name: "Scootaloo",
+            image: "scootaloo.png",
+            foundMatch: false
+        },
+        {
+            id: 20,
+            name: "Scootaloo",
+            image: "scootaloo.png",
+            foundMatch: false
+        },
+        {
+            id: 21,
+            name: "Shining Armor",
+            image: "ShiningArmor.jpeg",
+            foundMatch: false
+        },
+        {
+            id: 22,
+            name: "Shining Armor",
+            image: "ShiningArmor.jpeg",
+            foundMatch: false
+        },
+        {
+            id: 23,
+            name: "Spike",
+            image: "Spike.png",
+            foundMatch: false
+        },
+        {
+            id: 24,
+            name: "Spike",
+            image: "Spike.png",
+            foundMatch: false
+        },
+    ];
+}
